c3a86c1bd82e24ac4f1f444213d1dedb9f3024db
iontech <ikramuddinmiu@gmail.com>
2012-08-31 13:18:10 +0530

    Added code to recieve the intent in AnagramsActivity and Log it.

--- src/com/agiliq/anagen/AnagramsActivity.java
+++ src/com/agiliq/anagen/AnagramsActivity.java
@@ -1,14 +1,22 @@
 package com.agiliq.anagen;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class AnagramsActivity extends Activity {
 
+	private static String TAG = "AnagramsActivity.";
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anagrams);
+        
+        String localTAG = TAG.concat("onCreate");
+        
+        Intent intent=getIntent();
+        	Log.d(localTAG, intent.toString());
     }
-
 }
