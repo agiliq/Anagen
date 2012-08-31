78635426e951a59eca97b71c7e791ef83a7af113
iontech <ikramuddinmiu@gmail.com>
2012-08-31 13:37:17 +0530

    Added an ArrayAdapter which contains the recieved anagrams and set this adapter to the ListView.

--- src/com/agiliq/anagen/AnagramsActivity.java
+++ src/com/agiliq/anagen/AnagramsActivity.java
@@ -6,6 +6,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class AnagramsActivity extends Activity {
 
@@ -24,5 +26,9 @@ public class AnagramsActivity extends Activity {
         ArrayList<String> recievedAnagrams=intent.getStringArrayListExtra("MainActivity.-anagrams");
         	Log.d(localTAG+"-recievedAnagrams", recievedAnagrams.toString());
         
+        ArrayAdapter<String> anagramsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, recievedAnagrams);
+        	Log.d(localTAG+"-anagramsAdapter", anagramsAdapter.toString());
+        ListView anagramsList=(ListView)findViewById(R.id.anagramslist);
+        	anagramsList.setAdapter(anagramsAdapter);
     }
 }
